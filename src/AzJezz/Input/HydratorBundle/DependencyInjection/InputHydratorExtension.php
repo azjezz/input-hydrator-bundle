fee51336c49ecb62d188bd18fba0b487ee983a37
azjezz <azjezz@protonmail.com>
2020-10-31 20:58:02 +0100

    cs fix

--- src/AzJezz/Input/HydratorBundle/DependencyInjection/InputHydratorExtension.php
+++ src/AzJezz/Input/HydratorBundle/DependencyInjection/InputHydratorExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AzJezz\Input\HydratorBundle\DependencyInjection;
 
+use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -14,7 +15,7 @@ final class InputHydratorExtension extends Extension
     /**
      * Loads a specific configuration.
      *
-     * @throws \Exception If something went wrong.
+     * @throws Exception If something went wrong.
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
